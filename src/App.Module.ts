080f048f3f9f8ddd17a2912f780c200f63a51d45
chandansingh005 <chandan.singh@brauz.com>
2024-03-31 14:35:30 +0545

    Setup Appp Config + Database Config

--- src/App.Module.ts
+++ src/App.Module.ts
@@ -1,15 +1,12 @@
-
 import { Module, NestModule } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './App.Controller';
+import { AppConfigModule } from './config/AppConfig.Module';
 
 @Module({
-  imports: [
-    ThrottlerModule.forRoot(),
-    EventEmitterModule.forRoot(),
-  ],
+  imports: [AppConfigModule, ThrottlerModule.forRoot(), EventEmitterModule.forRoot()],
   controllers: [AppController],
   providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
